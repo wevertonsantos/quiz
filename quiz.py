81021b9b78d7c8a8b298767a901020e54ed1b989
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-09 14:42:34 -0300

    melhorando função mostrar_menu

--- quiz.py
+++ quiz.py
@@ -1,17 +1,17 @@
 def main():
-    perguntas = ["Qual é o maior planeta do sistema solar?","Quem escreveu a peça 'Romeu e Julieta'?"] 
 
-    mostrar_menu()
+    if mostrar_menu():
+        ...
 
 def mostrar_menu():
     while True:
         try:
             escolha = int(input("Escolha uma opção:\n1. Jogar\n2. Sair\nOpção: "))
             if escolha == 1:
-                ...
+                return True
             elif escolha == 2:
                 print("Obrigado. Até logo!")
-                break
+                return False
             else:
                 print("Não existe essa opção, escolha outra.")
         except ValueError:
