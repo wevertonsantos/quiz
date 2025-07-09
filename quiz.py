76fe1126527eb80e4225a67547c7ca7efe7fb194
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-09 15:15:08 -0300

    criando função perguntar questão para retornar a resposta e validar resposta, com isso vou alterar a lista para dicionário.

--- quiz.py
+++ quiz.py
@@ -1,8 +1,9 @@
 def main():
+    pontos = 0
 
     if mostrar_menu():
         for pergunta in carregar_perguntas():
-            print(pergunta)
+            validar_resposta(perguntar_questao(pergunta,carregar_perguntas()),'c')
 
 def mostrar_menu():
     while True:
@@ -19,6 +20,31 @@ def mostrar_menu():
             print("Você digitou algo errado. Tente novamente!")
 
 def carregar_perguntas():
+    lista_perguntas = {
+        1:{
+            'pergunta':"Qual é o maior planeta do sistema solar?",
+            'resposta':""
+        }
+    }
     return ["Qual é o maior planeta do sistema solar?","Quem escreveu a peça 'Romeu e Julieta'?","Em que país se localiza a Torre Eiffel?","Qual é o elemento químico representado pelo símbolo 'O'?","Quem foi o primeiro presidente do Brasil?"]
+
+def perguntar_questao(questao,perguntas):
+    if perguntas.index(questao) == 0:
+        print(questao)
+        resposta = input("A - Marte\nB - Saturno\nC - Júpiter\nOpção: ").strip().lower()
+        if resposta == "a" or resposta == "b" or resposta == "c":
+            return resposta
+        else:
+            print("Resposta não existe.")
+    elif perguntas.index(questao) == 1:
+        print(questao)
+        resposta = input("A - Charles Dickens\nB - William Shakespeare\nC - Machado de Assis\nOpção: ").strip().lower()
+        if resposta == "a" or resposta == "b" or resposta == "c":
+            return resposta
+        else:
+            print("Resposta não existe.")
+
+def validar_resposta(resposta, resposta_certa):
+    return resposta == resposta_certa
 
 main()
